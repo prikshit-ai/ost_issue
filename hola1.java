19e6395980c3f4658fee3400672f96e6e474d349
prikshitgaur772@gmail.com <prikshitgaur772@gmail.com>
2026-03-25 11:07:06 +0530

    initial commit

---
+++ hola1.java
@@ -0,0 +1,23 @@
+public class hola1{
+
+    public static void main(String[] args) {
+       //Scanner cf = new Scanner(System.in) ;
+       //System.out.println("type number ");
+
+       //int num = cf.nextInt();
+       for(int i = 0 ; i<26 ; i++){
+       if(i%3 == 0 ){
+            System.out.println("fizz :" + i);
+       }
+       else if(i%4==0){
+        System.out.println("buzz :" + i );
+
+       }
+       else if(i%5==0){
+        System.out.println("fizz buzz :" + i);
+
+       }
+    }
+        
+    }
+}
